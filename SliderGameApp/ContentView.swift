9bb5c399af2bac9df4313076511bdc9a436f3b57
Aleksandr Kretov <kretov.1988@gmail.com>
2022-05-23 17:15:58 +0500

    Initial Commit

---
+++ SliderGameApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderGameApp
+//
+//  Created by Aleksandr Kretov on 23.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SliderGameApp/SliderGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameAppApp.swift
+//  SliderGameApp
+//
+//  Created by Aleksandr Kretov on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
