2e1aa5971d77d7996ad1a193ae062fb6092b0119
Aleksandr Kretov <kretov.1988@gmail.com>
2022-05-23 22:50:45 +0500

    Release

--- SliderGameApp/ContentView.swift
+++ SliderGameApp/ContentView.swift
@@ -8,25 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sliderValue: Double = 50
-    @State private var alertPresented = false
-    @State private var targetNumber = Int.random(in: 0...100)
-    @State private var color = UIColor.red.withAlphaComponent(1.0)
     
+    @State private var sliderValue: Double = 50
+    @State private var targetNumber = Int.random(in: 0...100)
+    @State private var alertPresented = false
+
     var body: some View {
         VStack {
             Spacer()
             VStack(spacing: 32) {
-                Text("Подвиньте слайдер как можно ближе к: \(targetNumber)")
-                    .lineLimit(2)
-                UISliderView(value: $sliderValue, targetNumber: $targetNumber, color: .red.withAlphaComponent(sliderValue / Double(targetNumber)))
-                Button(action: showAlert) {
-                    Text("Проверить результат")
+                Text("Подвиньте слайдер как можно ближе к числу \(targetNumber)")
+                    .multilineTextAlignment(.center)
+                HStack {
+                    Text("0")
+                    SliderView(value: $sliderValue, targetNumber: targetNumber)
+                    Text("100")
                 }
-                Button("Начать заново") {newGame()}
+                Button(action: showAlert) {
+                    Text("Проверить результат").bold().foregroundColor(.green)
+                }
+                Button(action: newGame) {
+                    Text("Начать заново").bold()
+                }
             }
             Spacer()
         }
+        .padding()
         .alert("Результат", isPresented: $alertPresented, actions: {}) {
             Text("Вы набрали \(computeScore()) очков")
         }
@@ -35,12 +42,12 @@ struct ContentView: View {
     private func showAlert() {
         alertPresented.toggle()
     }
-    
+
     private func newGame() {
         sliderValue = Double.random(in: 0...100)
         targetNumber = Int.random(in: 0...100)
     }
-    
+
     private func computeScore() -> Int {
         let difference = abs(targetNumber - lround(sliderValue))
         return 100 - difference
--- SliderGameApp/SliderView.swift
+++ SliderGameApp/SliderView.swift
@@ -7,55 +7,53 @@
 
 import SwiftUI
 
-struct UISliderView: UIViewRepresentable {
-
-    @Binding var value: Double
-    @Binding var targetNumber: Int
-    var color: UIColor
+struct SliderView: UIViewRepresentable {
     
-    private let bounds = UIScreen.main.bounds
+    @Binding var value: Double
+    var targetNumber: Int
+
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider()
-        slider.thumbTintColor = color
-        slider.maximumValue = 100
         slider.minimumValue = 0
-        slider.minimumTrackTintColor = .blue
+        slider.maximumValue = 100
         slider.addTarget(
             context.coordinator,
             action: #selector(Coordinator.sliderValueChanged),
             for: .valueChanged)
         return slider
     }
-    
+
     func updateUIView(_ uiView: UISlider, context: Context) {
+        let thumbOpacity = value / Double(targetNumber)
         uiView.value = Float(value)
+        uiView.thumbTintColor = .red.withAlphaComponent(thumbOpacity)
     }
-    
-    
+
     func makeCoordinator() -> Coordinator {
-        Coordinator(value: $value, targetNumber: $targetNumber)
+        Coordinator(value: $value, targetNumber: targetNumber)
     }
 }
 
-extension UISliderView {
+extension SliderView {
+    
     final class Coordinator: NSObject {
         @Binding var value: Double
-        @Binding var targetNumber: Int
-     
-        init(value: Binding<Double>, targetNumber: Binding<Int>) {
+        var targetNumber: Int
+
+        init(value: Binding<Double>, targetNumber: Int) {
             self._value = value
-            self._targetNumber = targetNumber
+            self.targetNumber = targetNumber
         }
-        
+
         @objc func sliderValueChanged(_ sender: UISlider) {
             value = Double(sender.value)
         }
     }
 }
 
-
 struct UISliderView_Previews: PreviewProvider {
     static var previews: some View {
-        UISliderView(value: .constant(10), targetNumber: .constant(100), color: .red)
+        SliderView(value: .constant(35), targetNumber: 100
+        )
     }
 }
